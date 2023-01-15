9600cefc1fa91140a5a2cd93f161c223270c71e7
kpssnik <kpssdeveloper@gmail.com>
2023-01-16 00:52:59 +0200

    В слое application создан класс под DI. Метод класса вызван в мейне, тем самым инжектя все сервисы из слоя

--- BikeShop.Backend/BikeShop.Workspace/BikeShop.Workspace.WebApi/Program.cs
+++ BikeShop.Backend/BikeShop.Workspace/BikeShop.Workspace.WebApi/Program.cs
@@ -1,4 +1,10 @@
+using BikeShop.Workspace.Application;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Инъекция сервисов из слоя Application
+builder.Services.AddApplication();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
