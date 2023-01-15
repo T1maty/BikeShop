32623790ab77604ffbc392c27d27360c38f7caed
kpssnik <kpssdeveloper@gmail.com>
2023-01-15 23:52:36 +0200

    Начальное заполнение Application(Services) слоя, создана папка для интерфейсов, реализован интерфейс под дбконтекст

---
+++ BikeShop.Backend/BikeShop.Workspace/BikeShop.Workspace.Application/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BikeShop.Workspace.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BikeShop.Workspace.Application.Interfaces;
+
+// Интерфейс для единственного dbContext'а программы
+public interface IApplicationDbContext
+{
+    DbSet<User> Users { get; set; }
+    DbSet<UserRole> UserRoles { get; set; }
+    DbSet<Shop> Shops { get; set; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
