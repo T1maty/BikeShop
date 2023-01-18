fa691be0cade13634835d082eff4df4f3f08563d
kpssnik <kpssdeveloper@gmail.com>
2023-01-18 12:10:08 +0200

    Создан GroupController. Реализовано получение списка групп по айди магазина

---
+++ BikeShop.Backend/BikeShop.Workspace/BikeShop.Workspace.Application/CQRS/Queries/WorkGroup/GetWorkGroupByShopId/WorkGroupListModel.cs
@@ -0,0 +1,6 @@
+﻿namespace BikeShop.Workspace.Application.CQRS.Queries.WorkGroup.GetWorkGroupByShopId;
+
+public class WorkGroupListModel
+{
+    public IList<Domain.Entities.WorkGroup> WorkGroups { get; set; }
+}
---
+++ BikeShop.Backend/BikeShop.Workspace/BikeShop.Workspace.WebApi/Controllers/GroupController.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using BikeShop.Workspace.Application.CQRS.Queries.WorkGroup.GetWorkGroupByShopId;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BikeShop.Workspace.WebApi.Controllers;
+
+[ApiController]
+[Route("group")]
+public class GroupController : ControllerBase
+{
+    private readonly IMapper _mapper;
+    private readonly IMediator _mediator;
+
+    public GroupController(IMapper mapper, IMediator mediator)
+    {
+        _mapper = mapper;
+        _mediator = mediator;
+    }
+
+    // Получение групп услуг по айди магазина
+    [HttpGet("getbyshopid/{id:int}")]
+    public async Task<ActionResult<WorkGroupListModel>> GetWorkGroupsByShopId(int id)
+    {
+        // Создаю запрос
+        var query = new GetWorkGroupByShopIdQuery { ShopId = id };
+
+        // Посылаю запрос и получаю модель с листом групп услуг
+        var result = await _mediator.Send(query);
+
+        return Ok(result);
+    }
+}
