58e09bb11249cbfe1e68a7f838471fb5a286be68
yasinskiYan <yasinskijan161@gmail.com>
2023-01-19 15:53:02 +0300

    feat: added header component

--- BikeShop.Frontend/webclient/src/App.tsx
+++ BikeShop.Frontend/webclient/src/App.tsx
@@ -1,48 +1,43 @@
 import React, {
   // lazy,
-  Suspense
-} from 'react';
-import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
+  Suspense,
+} from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
-
-import MainPage from './components/pages/MainPage';
-
+import MainPage from "./components/pages/MainPage";
+import Header from "./components/Header/Header";
 
 const darkTheme = createTheme({
   palette: {
-    mode: 'dark',
-  }
+    mode: "dark",
+  },
 });
 
 const App: React.FC = () => {
-
-
-
   return (
     <div className="App">
       <ThemeProvider theme={darkTheme}>
         <CssBaseline />
         <Router>
-          <div>Header</div>
+          <Header />
           <Suspense>
             <Routes>
-              <Route path='/' element={<MainPage />} />
+              <Route path="/" element={<MainPage />} />
 
-
-              <Route path='*' element={<div>Not Found</div>} />
+              <Route path="*" element={<div>Not Found</div>} />
             </Routes>
           </Suspense>
         </Router>
       </ThemeProvider>
     </div>
   );
-}
+};
 
 export default App;
--- BikeShop.Frontend/webclient/src/index.tsx
+++ BikeShop.Frontend/webclient/src/index.tsx
@@ -1,11 +1,13 @@
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { StyledEngineProvider } from "@mui/material";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
+  <StyledEngineProvider injectFirst>
     <App />
+  </StyledEngineProvider>
 );
-
