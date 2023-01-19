4c4703b3db50e2d6c41952d6bb47f2dcf7d41290
yasinskiYan <yasinskijan161@gmail.com>
2023-01-20 02:25:38 +0300

    fix: get rid of styles

--- BikeShop.Frontend/webclient/src/components/Header/Header.tsx
+++ BikeShop.Frontend/webclient/src/components/Header/Header.tsx
@@ -3,10 +3,7 @@ import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import Badge from "@mui/material/Badge";
-import { AppBar, Typography } from "@mui/material";
-import { Box } from "@mui/system";
-
-import styles from "./Header.module.css";
+import { AppBar, Grid, Typography } from "@mui/material";
 
 const Header: React.FC = () => {
   const [currentTime, setCurrentTime] = useState(new Date());
@@ -28,46 +25,55 @@ const Header: React.FC = () => {
   }, [currentTime]);
 
   return (
-    <AppBar position="static" color="primary" className={styles.container}>
-      <Box
-        component="div"
-        className={styles.marketName}
-        sx={{ margin: "0 5px" }}
-      >
-        <IconButton
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          className={styles.burger}
+    <AppBar position="static" color="primary" sx={{ p: "12px 25px" }}>
+      <Grid container direction="row" alignItems="center">
+        <Grid
+          item
+          alignItems="center"
+          direction="row"
+          xs={4}
+          sx={{ display: "flex" }}
         >
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="subtitle1">market's name</Typography>
-      </Box>
-      <Typography
-        variant="subtitle1"
-        className={styles.time}
-        sx={{ margin: "0 5px" }}
-      >
-        {time}
-      </Typography>
-      <Box
-        component="div"
-        className={styles.employeeName}
-        sx={{ margin: "0 5px" }}
-      >
-        <Typography variant="subtitle1">employee's name</Typography>
-        <IconButton
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          className={styles.ring}
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: "20px" }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="subtitle1" noWrap>
+            market's name
+          </Typography>
+        </Grid>
+        <Grid
+          item
+          xs={4}
+          alignItems="center"
+          justifyContent="center"
+          sx={{ display: "flex" }}
         >
-          <Badge badgeContent={3} color="secondary">
-            <NotificationsIcon />
-          </Badge>
-        </IconButton>
-      </Box>
+          <Typography variant="subtitle1" noWrap>
+            {time}
+          </Typography>
+        </Grid>
+        <Grid
+          item
+          xs={4}
+          alignItems="center"
+          justifyContent="center"
+          sx={{ display: "flex" }}
+        >
+          <Typography variant="subtitle1" noWrap sx={{ mr: "20px" }}>
+            employee's name
+          </Typography>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            <Badge badgeContent={3} color="secondary">
+              <NotificationsIcon />
+            </Badge>
+          </IconButton>
+        </Grid>
+      </Grid>
     </AppBar>
   );
 };
