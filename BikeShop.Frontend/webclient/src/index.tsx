b6958421e3c43af449504d27bd509928d3822ecb
yasinskiYan <yasinskijan161@gmail.com>
2023-01-20 02:26:54 +0300

    fix: remove injector

--- BikeShop.Frontend/webclient/src/index.tsx
+++ BikeShop.Frontend/webclient/src/index.tsx
@@ -1,13 +1,8 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { StyledEngineProvider } from "@mui/material";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(
-  <StyledEngineProvider injectFirst>
-    <App />
-  </StyledEngineProvider>
-);
+root.render(<App />);
