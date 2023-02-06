4765ffdb90e716042febca6e7ecb41a52c7dc9fd
banderveloper <kpssdeveloper@gmail.com>
2023-02-06 11:55:54 +0200

    Создан почти пустой микросервис Payments, создано несколько сущностей

---
+++ BikeShop.Backend/BikeShop.Payments/BikeShop.Payments.Domain/Entities/Currency.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BikeShop.Payments.Domain.Entities;
+
+public class Currency : BaseEntity
+{
+    public string Name { get; set; }
+    public string Symbol { get; set; }
+    public decimal Coefficient { get; set; }
+    public bool IsBaseCurrency { get; set; } = false;
+
+    [JsonIgnore] public IList<CurrencyHistory> CurrencyHistories { get; set; }
+}
---
+++ BikeShop.Backend/BikeShop.Payments/BikeShop.Payments.Domain/Entities/CurrencyHistory.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BikeShop.Payments.Domain.Entities;
+
+public class CurrencyHistory : BaseEntity
+{
+    public int CurrencyId { get; set; }
+    [JsonIgnore] public Currency Currency { get; set; }
+
+    public decimal Coefficient { get; set; }
+}
