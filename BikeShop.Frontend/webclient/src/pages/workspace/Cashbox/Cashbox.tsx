3e8baebd4d6f92eeed8f629e7d07bb1ac26419ef
Crash2022 <crash555@mail.ru>
2023-02-09 16:18:12 +0500

    add: Cashbox styles done

--- BikeShop.Frontend/webclient/src/pages/workspace/Cashbox/Cashbox.tsx
+++ BikeShop.Frontend/webclient/src/pages/workspace/Cashbox/Cashbox.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import {Box, Container, Grid, Paper, Stack} from "@mui/material";
+import {Box, Container, Grid, Paper, Stack, TextField} from "@mui/material";
 import Button from "../../../shared/ui/Button/Button";
 import s from "./Cashbox.module.css"
+import Input from 'shared/ui/Input/Input';
 
 export const Cashbox = () => {
 
@@ -22,53 +23,47 @@ export const Cashbox = () => {
                             borderRadius: 2,
                         }}
                     >
-                        <Paper>
-                            <Container sx={{width: '100%', height: '100%'}}/>
-
-                            <Box
-                                sx={{
-                                    width: '100%',
-                                    height: 'fit-content',
-                                    p: 2,
-                                    display: 'flex',
-                                    justifyContent: 'center',
-                                    flexWrap: 'wrap'
-                                }}
-                            >
-                                <div className={s.buttonsTableBlock}>
-                                    <Button text={'Стол 1'}/>
-                                    <Button text={'Стол 2'}/>
-                                    <Button text={'Стол 3'}/>
-                                    <Button text={'Стол 4'}/>
-                                </div>
-                            </Box>
-
-                        </Paper>
+                        <Box
+                            sx={{
+                                width: '100%',
+                                height: 'fit-content',
+                                p: 2,
+                                display: 'flex',
+                                justifyContent: 'center',
+                                flexWrap: 'wrap'
+                            }}
+                            className={s.tableBlock_buttons}
+                        >
+                            <Button text={'Стол 1'}/>
+                            <Button text={'Стол 2'}/>
+                            <Button text={'Стол 3'}/>
+                            <Button text={'Стол 4'}/>
+                        </Box>
                     </Box>
                     <Box
                         sx={{
                             width: '100%',
-                            height: 'fit-content',
+                            // height: 'fit-content',
+                            height: '100vh',
                             p: 2,
                             backgroundColor: 'grey.800',
                             borderRadius: 2,
                         }}
                     >
                         <Paper>
-                            {/*<Container sx={{width: '100%', height: '75vh'}}/>*/}
                             <div className={s.clientBox}>
-                                <h3 style={{textAlign: 'center'}}>Клиент</h3>
+                                <h3 style={{textAlign: 'center', marginTop: '0px'}}>Клиент</h3>
                                 <p>Панкратов Егвений Владимирович</p>
                                 <p>Номер телефона</p>
                                 <p>Почта</p>
                                 <p>Баланс: 0</p>
                             </div>
                         </Paper>
-                        <div className={s.clientBox_funcBlock}>
-                            <div className={s.clientBox_funcBlock_chooseBtn}>
+                        <div className={s.clientBox_buttons}>
+                            <div className={s.clientBox_buttons_chooseBtn}>
                                 <Button text={'Выбрать клиента'}/>
                             </div>
-                            <div className={s.clientBox_funcBlock_cancelBtn}>
+                            <div className={s.clientBox_buttons_cancelBtn}>
                                 <Button text={'X'}/>
                             </div>
                         </div>
@@ -77,43 +72,47 @@ export const Cashbox = () => {
 
                 <Box
                     sx={{
-                        width: {xs: '100%', sm: '70%'},
+                        width: {xs: '100%', sm: '100%'},
                         p: 2,
                         backgroundColor: 'grey.800',
                         borderRadius: 2,
                     }}
                 >
+                    <div className={s.cashbox_rightBlock}>
+                        <div className={s.cashbox_rightBlock_header}>
+                            <div className={s.cashbox_header_chooseBtn}>
+                                <Button text={'Выбрать товары'}/>
+                            </div>
+                            <div className={s.cashbox_header_searchInput}>
+                                <Input placeholder={'Поиск...'}/>
+                            </div>
+                        </div>
+                    </div>
                     <Paper>
                         <Container sx={{width: '100%', height: '100vh'}}/>
                     </Paper>
+                    <div className={s.cashbox_rightBlock_bottom}>
+                        <div className={s.cashbox_rightBlock_bottom_buttonsLeft}>
+                            <div className={s.cashbox_rightBlock_bottom_cancelBtn}>
+                                <Button text={'X'}/>
+                            </div>
+                            <div className={s.cashbox_rightBlock_bottom_sumNoDiscount}>
+                                Без скидки
+                            </div>
+                            <div className={s.cashbox_rightBlock_bottom_discount}>
+                                Скидка
+                            </div>
+                        </div>
+                        <div className={s.cashbox_rightBlock_bottom_Sum}>
+                            Итоговая сумма
+                        </div>
+                    </div>
+                    <div className={s.cashbox_rightBlock_bottom_buttonResult}>
+                        <Button text={'К оплате'}/>
+                    </div>
                 </Box>
 
             </Stack>
         </Container>
     );
 };
-
-
-// <Grid container spacing={2}>
-//     <Grid item xs={4}>
-//         <Paper
-//             sx={{
-//                 height: '50%',
-//                 width: '100%',
-//                 backgroundColor: (theme) =>
-//                     theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-//             }}
-//             {/*//
-//                     <div style={{backgroundColor: 'green', margin: '8px'}}>
-//                         hello
-//                     </div>
-//                     <div style={{backgroundColor: 'blue'}}>
-//                         hello
-//                     </div>
-//                 </Grid>
-//                 <Grid item xs={8}>
-//                     <div style={{backgroundColor: 'yellow'}}>
-//                         hello
-//                     </div>
-//                 </Grid>
-//             </Grid>
