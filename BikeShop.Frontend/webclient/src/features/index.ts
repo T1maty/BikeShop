e4bef1a64a014f98a6c6bb536b5f357ca572623c
NoMadrilius <122482610+NoMadrilius@users.noreply.github.com>
2023-02-11 16:29:14 +0300

    Update index.ts

--- BikeShop.Frontend/webclient/src/features/index.ts
+++ BikeShop.Frontend/webclient/src/features/index.ts
@@ -1,6 +1,5 @@
 export {default as HeaderUserMenu} from './HeaderUserMenu/HeaderUserMenu';
 export {default as CreateTagModal} from './CreateTagModal/CreateTagModal';
 
-export {default as useCreateTagModal} from './CreateTagModal/CreateTagModalStore';
 export {default as CreateProductModal} from './createProductModal/createProductModal';
 export {default as useCreateProductModalStore} from './createProductModal/createProductModalStore';
