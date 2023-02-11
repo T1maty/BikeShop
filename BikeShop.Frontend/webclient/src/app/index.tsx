230bca6fda36c3136d409a92004090d9153dcbcc
Crash2022 <crash555@mail.ru>
2023-02-11 17:09:16 +0500

    fix: change main font

--- BikeShop.Frontend/webclient/src/app/index.tsx
+++ BikeShop.Frontend/webclient/src/app/index.tsx
@@ -1,10 +1,10 @@
 import React, {Suspense} from "react";
 import {createTheme, ThemeProvider} from "@mui/material/styles";
 
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
+// import "@fontsource/roboto/300.css";
+// import "@fontsource/roboto/400.css";
+// import "@fontsource/roboto/500.css";
+// import "@fontsource/roboto/700.css";
 import CssBaseline from "@mui/material/CssBaseline";
 import {RouterProvider} from "react-router-dom";
 import {Routes} from "./allRoutes";
--- BikeShop.Frontend/webclient/src/index.tsx
+++ BikeShop.Frontend/webclient/src/index.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import './app/index.css';
+// import './app/_index.scss';
 import App from './app';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
