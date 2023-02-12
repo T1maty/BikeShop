54ed7358c5f55f8ca06aab96fc6f4ac66450fadb
Crash2022 <crash555@mail.ru>
2023-02-12 19:17:48 +0500

    refactor: imports for components

--- BikeShop.Frontend/webclient/src/features/index.ts
+++ BikeShop.Frontend/webclient/src/features/index.ts
@@ -1,3 +1,6 @@
 export {default as HeaderUserMenu} from './HeaderUserMenu/HeaderUserMenu';
 export {default as CreateTagModal} from './CreateTagModal/CreateTagModal';
 export {default as CreateProductModal} from './CreateProductModal/CreateProductModal';
+export {default as ChooseClientModal} from './ChooseClientModal/ChooseClientModal';
+export {default as ClientCard} from './ClientCard/ClientCard';
+export {default as TableProductItem} from './TableProductItem/TableProductItem';
