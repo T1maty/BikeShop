e62bb7209932270aaee00135dc07c1cd19311c3d
Crash2022 <crash555@mail.ru>
2023-02-12 11:47:11 +0500

    fix: MUI input styles

--- BikeShop.Frontend/webclient/src/shared/ui/Input/Input.tsx
+++ BikeShop.Frontend/webclient/src/shared/ui/Input/Input.tsx
@@ -1,8 +1,8 @@
-import { TextField } from '@mui/material';
-import { IInput } from './IInput';
+import {TextField} from '@mui/material';
+import {IInput} from './IInput';
 
-const InputUI = ({ placeholder }: IInput) => {
-  return <TextField placeholder={placeholder} />;
+const InputUI = ({placeholder}: IInput) => {
+    return <TextField placeholder={placeholder} fullWidth={true}/>;
 };
 
 export default InputUI;
