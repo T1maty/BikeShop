560c4d403932f51d52c4cb7231ea0d7d01937ef9
onemore8762 <denis.churkin.afikei@mail.ru>
2023-02-21 03:29:06 +0300

    fix: Custom button

--- BikeShop.Frontend/webclient/src/shared/ui/Button/Button.tsx
+++ BikeShop.Frontend/webclient/src/shared/ui/Button/Button.tsx
@@ -1,24 +1,16 @@
 import React, {type ButtonHTMLAttributes, type FC, MouseEvent} from 'react'
 import cls from './Button.module.scss'
-import {createRipple} from "./lib/ripple";
-
-type RGB = `rgb(${number}, ${number}, ${number})`;
-type RGBA = `rgba(${number}, ${number}, ${number}, ${number})`;
-type HEX = `#${string}`;
-
-
-type defaultColor = 'white' | 'black'
-type Color = RGB | RGBA | HEX | defaultColor
+import {Animate, createRipple} from "./lib/ripple";
+import clsx from "clsx";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-    className?: string
-    variant?: string
-    colorText?: Color
+    // variant?: string
+    onAnimate?: boolean
+    viewAnimate?: Animate
 }
 
 /**
  * @param props is default PropsButton
- * @param colorText white or black, or custom RGB, RGBA, HEX
  * @param className NOT USE, NOT WORK
  * @return customButton jsx
  */
@@ -26,26 +18,25 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 export const Button: FC<ButtonProps> = (
     {
         children,
-        style,
-        variant,
-        className ,
-        colorText = '',
+        // variant,
+        className,
         onClick,
+        viewAnimate,
+        onAnimate = true,
         ...otherProps
     }
 ) => {
 
     const onClickHandler = (e: MouseEvent<HTMLButtonElement>) => {
         onClick && onClick(e)
-        createRipple(e, cls)
+        onAnimate && createRipple(e, cls, viewAnimate)
     }
 
     return (
         <button
             type='button'
-            style={{color: colorText, ...style}}
             onClick={onClickHandler}
-            className={cls.btn + ' ' + className}
+            className={clsx(cls.btn, className)}
             {...otherProps}
         >
             {children}
