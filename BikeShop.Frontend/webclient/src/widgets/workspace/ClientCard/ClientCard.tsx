02635eaaef258384f28471b3248eab4795ff40df
Crash2022 <crash555@mail.ru>
2023-02-21 18:58:01 +0500

    refactor: ClientCard text

--- BikeShop.Frontend/webclient/src/widgets/workspace/ClientCard/ClientCard.tsx
+++ BikeShop.Frontend/webclient/src/widgets/workspace/ClientCard/ClientCard.tsx
@@ -11,11 +11,16 @@ const ClientCard: React.FC<ClientCardProps> = ({user}) => {
     return (
         <div className={s.clientCard_background}>
             <h3>Клиент</h3>
-            <p><span>ФИО:</span> {user.lastName} {user.firstName} {user.patronymic}</p>
-            <p><span>Номер телефона:</span> {user.phoneNumber}</p>
+            <p>
+                <span>ФИО:</span> {''}
+                {user.lastName ? user.lastName : 'Клиент'} {''}
+                {user.firstName ? user.firstName : 'не'} {''}
+                {user.patronymic ? user.patronymic : 'выбран'}
+            </p>
+            <p><span>Номер телефона:</span> {user.phoneNumber ? user.phoneNumber : 'Клиент не выбран'}</p>
             {/*<p><span>Почта:</span> {email}</p>*/}
-            <p><span>Баланс:</span> {user.balance}</p>
-            <p><span>Кредитный лимит:</span> {user.creditLimit}</p>
+            <p><span>Баланс:</span> {user.balance ? user.balance : 0}</p>
+            <p><span>Кредитный лимит:</span> {user.creditLimit ? user.creditLimit : 0}</p>
         </div>
     )
 }
