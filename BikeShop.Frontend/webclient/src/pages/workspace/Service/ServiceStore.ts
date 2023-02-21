2925edd461536fb2174742e0342d755fe60ac979
Crash2022 <crash555@mail.ru>
2023-02-22 00:58:37 +0500

    fix: filter services, button styles

--- BikeShop.Frontend/webclient/src/pages/workspace/Service/ServiceStore.ts
+++ BikeShop.Frontend/webclient/src/pages/workspace/Service/ServiceStore.ts
@@ -6,25 +6,23 @@ import {ServiceItem} from "../../../entities/responses/ServiceItem";
 import {$api} from "../../../shared";
 import {IUser} from "../../../entities";
 import {CreateService} from "../../../entities/requests/CreateService";
-import {ServiceStatusType} from "./Service";
 
 interface ServiceStore {
     isLoading: boolean
     setIsLoading: (value: boolean) => void
     user: IUser
     setUser: (user: IUser) => void
-
     services: ServiceItem[]
     getAllServices: () => any // надо исправить тип
-    getFilteredServices: () => any // надо исправить тип
     filteredServices: ServiceItem[]
     setFilteredServices: (filteredServices: ServiceItem[]) => void
-
+    getFilteredServices: () => any // надо исправить тип
     addNewService: (data: any) => any // надо исправить тип
     // updateService: (data: any) => any // надо исправить тип
+    // updateServiceStatus: (data: any) => any // надо исправить тип
 }
 
-export interface updateServiceStatus {
+export interface UpdateServiceStatus {
     serviceId: number
     newStatus: number
 }
@@ -49,6 +47,7 @@ const useService = create<ServiceStore>()(/*persist(*/devtools(immer((set) => ({
             set({isLoading: false});
         })
     },
+
     filteredServices: [],
     setFilteredServices: (filteredServices: ServiceItem[]) => set({
         filteredServices: filteredServices
