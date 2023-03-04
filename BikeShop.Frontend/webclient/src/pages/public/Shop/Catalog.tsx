59e85e3dd3de23fe43e4f8d6711dabd777c9430c
Crash2022 <crash555@mail.ru>
2023-03-04 16:20:35 +0500

    add: shop catalog styles

--- BikeShop.Frontend/webclient/src/pages/public/Shop/Catalog.tsx
+++ BikeShop.Frontend/webclient/src/pages/public/Shop/Catalog.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
-import s from "./Catalog.module.scss";
+import s from './Catalog.module.scss'
 
 export const Catalog = () => {
     return (
         <div className={s.catalog_mainBox}>
-            <div>content new</div>
+            <div className={s.catalog_left}>Категории</div>
+            <div className={s.catalog_right}>
+                <div className={s.right_cloudCategory}>
+                    <div className={s.cloudCategory_title}>Облако категорий</div>
+                    <div className={s.cloudCategory_content}>Категории</div>
+                </div>
+                <div className={s.right_filters}>
+                    <div className={s.filters_buttons}>
+                        <div>Сначала:</div>
+                        <div>1</div>
+                        <div>2</div>
+                        <div>3</div>
+                        <div>4</div>
+                    </div>
+                    <div className={s.filters_listType}>
+                        <div>1</div>
+                        <div>2</div>
+                    </div>
+                </div>
+                <div className={s.right_content}>Контент</div>
+                <div className={s.right_paginator}>Пагинатор</div>
+            </div>
         </div>
     )
 }
--- BikeShop.Frontend/webclient/src/pages/public/Shop/ShopHeader.tsx
+++ BikeShop.Frontend/webclient/src/pages/public/Shop/ShopHeader.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import s from './ShopHeader.module.scss'
+import burgerMenu from "../../../shared/assets/shop/icons/menu.png";
+import search from "../../../shared/assets/shop/icons/search.png";
+import cart from "../../../shared/assets/shop/icons/cart.png";
+import language from "../../../shared/assets/shop/icons/lang.png";
+import profile from "../../../shared/assets/shop/icons/profile.png";
 
 export const ShopHeader = () => {
     return (
         <div className={s.shop_header}>
-            header
+            <div className={s.shop_header_left}>
+                <img src={burgerMenu} alt="burger-menu"/>
+            </div>
+            <div className={s.shop_header_right}>
+                <div><img src={search} alt="search-logo"/></div>
+                <div><img src={cart} alt="cart-logo"/></div>
+                <div><img src={language} alt="language-logo"/></div>
+                <div><img src={profile} alt="profile-logo"/></div>
+            </div>
         </div>
     );
 };
