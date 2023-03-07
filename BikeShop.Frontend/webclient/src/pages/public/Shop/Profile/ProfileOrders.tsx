987b6ebb34a18aba155eca63d1d1ab4b6c294a87
Crash2022 <crash555@mail.ru>
2023-03-08 01:34:52 +0500

    add: profile order styles v2

--- BikeShop.Frontend/webclient/src/pages/public/Shop/Profile/ProfileOrders.tsx
+++ BikeShop.Frontend/webclient/src/pages/public/Shop/Profile/ProfileOrders.tsx
@@ -1,35 +1,179 @@
 import React, {useState, useEffect} from 'react'
 import s from './ProfileOrders.module.scss'
+import {MenuItem} from '@mui/material';
+import {ControlledInput} from '../../../../shared/ui';
+import {Errors} from '../../../../entities/errors/workspaceErrors';
+import {useForm} from 'react-hook-form';
 
 type ProfileOrderStatus = 'WaitingPayment' | 'WaitingPackage' | 'Ready' | 'Canceled'
 
 export const ProfileOrders = () => {
 
-    const [isCollapsedItem, setIsCollapsedItem] = useState<boolean>(false)
+    // const [isCollapsedItem, setIsCollapsedItem] = useState<boolean>(false)
+
+    // тестовые данные
+    const img01 = 'https://wallpapercave.com/wp/wp2118883.jpg'
+    const img02 = 'https://i.pinimg.com/originals/74/cf/2e/74cf2eb33969be3c522581d9b48e376e.jpg'
+    const img03 = 'https://i.pinimg.com/originals/8c/ef/90/8cef90ae85a3276e873ba89b21ff34e1.jpg'
+    const img04 = 'https://i.pinimg.com/originals/c8/43/33/c8433307705031e7514bfd3d46cb0629.jpg'
+
+    const [items, setItems] = useState([
+        {
+            id: '1', status: 'Ожидает комплектации', sum: 999, isPay: true, img: img01, isCollapsed: false,
+            products: [{id: '1', product: 'Merida'}]
+        },
+        {
+            id: '2', status: 'Ожидает оплаты', sum: 1999, isPay: false, img: img02, isCollapsed: true,
+            products: [{id: '2', product: 'Specialized'}]
+        },
+        {
+            id: '3', status: 'Готов к выдаче', sum: 99, isPay: true, img: img03, isCollapsed: false,
+            products: [{id: '3', product: 'Sram'}]
+        },
+        {
+            id: '4', status: 'Отменён', sum: 99999, isPay: false, img: img04, isCollapsed: true,
+            products: [{id: '4', product: 'Shimano'}, {id: '5', product: 'Author'}]
+        },
+    ])
+
+    const InputStyles = {
+        color: 'black',
+        '.MuiOutlinedInput-notchedOutline': {
+            borderColor: 'black',
+        },
+        '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+            borderColor: 'black',
+        },
+        '&:hover .MuiOutlinedInput-notchedOutline': {
+            borderColor: 'black',
+        },
+        '.MuiSvgIcon-root ': {
+            fill: 'black',
+        },
+    }
+    const InputLabelProps = {style: { color: 'black'}}
+
+    // сбор данных с формы //
+    const formControl = useForm({
+        defaultValues: {
+            lastName: '',
+            firstName: '',
+            patronymic: '',
+        }
+    });
+
+    // const onSubmit: SubmitHandler<any> = (data: any) => {
+    //     addNewService(data).then((res: any) => {
+    //         clearSubmitInfo() // очистка полей
+    //         enqueueSnackbar('Ремонт добавлен', {variant: 'success', autoHideDuration: 3000})
+    //     }).catch((error: any) => {
+    //         let message = error(error.response.data.errorDescription).toString()
+    //         formControl.setError('name', {type: 'serverError', message: message})
+    //         enqueueSnackbar(message, {variant: 'error', autoHideDuration: 3000})
+    //         console.error(error.response.data)
+    //     })
+    // }
+
+    const collapseItem = (currentItemId: string) => {
+        console.log(currentItemId)
+        items.map(el => el.id === currentItemId ? {...el, isCollapsed: !el.isCollapsed} : el)
+    }
 
     return (
         <div className={s.profileOrders_mainBox}>
             <div className={s.profileOrders_title}>Мои заказы</div>
-            <div className={s.profileOrders_content} onClick={() => {setIsCollapsedItem(true)}}>
+            <div className={s.profileOrders_content}>
+                {
+                    items.map((item: any) => {
+                        return (
+                            <div className={s.profileOrders_orderItem} key={item.id}>
 
-                <div className={s.profileOrders_orderItem}>
-                    <div className={s.orderItem_status}>
-                        <div className={s.status_color}>i</div>
-                        <div className={s.status_data}>
-                            <div className={s.status_data_date}>№999 от 1 января 2023 года</div>
-                            <div className={s.status_data_text}>Ожидает комплектации</div>
-                        </div>
-                    </div>
-                    <div className={s.orderItem_sum}>
-                        <div className={s.sum_price}>Сумма: 999</div>
-                        <div className={s.sum_result}>Оплата: <span>Оплачено</span></div>
-                    </div>
-                    <div className={s.orderItem_icons}>
-                        <div className={s.icons_one}>i</div>
-                        <div className={s.icons_two}>i</div>
-                    </div>
-                </div>
-
+                                <div className={s.orderItem_mainPart}>
+                                    <div className={s.orderItem_status}>
+                                        <div className={s.status_color}>i</div>
+                                        <div className={s.status_data}>
+                                            <div className={s.status_data_date}>№999 от 1 января 2023 года</div>
+                                            <div className={s.status_data_text}>{item.status}</div>
+                                        </div>
+                                    </div>
+                                    <div className={s.orderItem_sum}>
+                                        <div className={s.sum_price}>Сумма: {item.sum}</div>
+                                        <div className={s.sum_result}>
+                                            Оплата: <span>{item.isPay ? 'Оплачено' : 'Не оплачено'}</span>
+                                        </div>
+                                    </div>
+                                    <div className={s.orderItem_icons}>
+                                        <div className={s.icons_image}>
+                                            <img src={item.img} alt='order-photo'/>
+                                        </div>
+                                        <div className={s.icons_isCollapsed} onClick={() => collapseItem(item.id)}>
+                                            {item.isCollapsed ? 'Свернуть <' : 'Развернуть >'}
+                                        </div>
+                                    </div>
+                                </div>
+                                {
+                                    item.isCollapsed ?
+                                        <div className={s.orderItem_collapsedPart}>
+                                            <div className={s.collapsedPart_title}>Информация о заказе:</div>
+                                            <div className={s.collapsedPart_deliveryType}>Способ получения: доставка</div>
+                                            <div className={s.collapsedPart_deliveryDetails}>
+                                                <div>
+                                                    <ControlledInput name={'delivery-company'}
+                                                                     label={'Грузовая'}
+                                                                     control={formControl}
+                                                                     rules={{required: Errors[0].name}}
+                                                                     sx={InputStyles}
+                                                                     InputLabelProps={InputLabelProps}
+                                                    />
+                                                </div>
+                                                <div>
+                                                    <ControlledInput name={'post-type'}
+                                                                     label={'Почта'}
+                                                                     control={formControl}
+                                                                     rules={{required: Errors[0].name}}
+                                                                     sx={InputStyles}
+                                                                     InputLabelProps={InputLabelProps}
+                                                    />
+                                                </div>
+                                                <div>
+                                                    <ControlledInput name={'post-type'}
+                                                                     label={'Почта'}
+                                                                     control={formControl}
+                                                                     rules={{required: Errors[0].name}}
+                                                                     sx={InputStyles}
+                                                                     InputLabelProps={InputLabelProps}
+                                                    />
+                                                </div>
+                                            </div>
+                                            <hr/>
+                                            <div className={s.collapsedPart_discount}>Скидка: не выбрана</div>
+                                            <hr/>
+                                            <div className={s.collapsedPart_products}>
+                                                <div className={s.products_title}>Товары:</div>
+                                                <div className={s.products_list}>
+                                                    {
+                                                        item.products.map((p: any) => {
+                                                            return (
+                                                                <div className={s.list_item} key={p.id}>
+                                                                    {p.product}
+                                                                </div>
+                                                            )
+                                                        })
+                                                    }
+                                                </div>
+                                            </div>
+                                            <div className={s.collapsedPart_sum}>
+                                                <div>Сумма: 999</div>
+                                                <div>Скидка: 1%</div>
+                                                <div>К оплате: 990</div>
+                                            </div>
+                                        </div>
+                                        : ''
+                                }
+                            </div>
+                        )
+                    })
+                }
             </div>
         </div>
     )
