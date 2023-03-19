87ae67103ed205fe89633503c7182c47e5f6d51b
NoMadrilius <122482610+NoMadrilius@users.noreply.github.com>
2023-03-20 01:45:46 +0300

    Контроллер для карты продуктов

---
+++ BikeShop.Backend/BikeShop.Products/BikeShop.Products.Application/Services/ProductCardService.cs
@@ -0,0 +1,24 @@
+﻿using BikeShop.Products.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeShop.Products.Application.Services
+{
+    public class ProductCardService : IProductCardService
+    {
+        private readonly IProductCardService _productCardService;
+
+        public ProductCardService(IProductCardService productCardService)
+        {
+            _productCardService = productCardService;
+        }
+
+        public async Task UpdateProductCard()
+        {
+
+        }
+    }
+}
---
+++ BikeShop.Backend/BikeShop.Products/BikeShop.Products.WebApi/Controllers/ProductCardController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BikeShop.Products.WebApi.Controllers
+{
+    [Produces("application/json")]
+    [ApiController]
+    [Route("productcard")]
+    public class ProductCardController : ControllerBase
+    {
+    }
+}
