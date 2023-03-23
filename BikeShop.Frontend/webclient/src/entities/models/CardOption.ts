49afeffbf00cd2d68295343230112fbb57914785
Crash2022 <crash555@mail.ru>
2023-03-23 10:13:03 +0500

    add: CardOption interface

---
+++ BikeShop.Frontend/webclient/src/entities/models/CardOption.ts
@@ -0,0 +1,29 @@
+export interface CardOption {
+    "option": {
+        "name": "123",
+        "id": 1,
+        "createdAt": "2023-03-09T22:03:08",
+        "updatedAt": "2023-03-16T22:03:08",
+        "enabled": true
+    },
+    "optionVariants": [
+        {
+            "optionId": 1,
+            "name": "321",
+            "optionName": "123",
+            "id": 1,
+            "createdAt": "2023-03-15T22:07:03",
+            "updatedAt": "2023-03-04T22:07:03",
+            "enabled": true
+        },
+        {
+            "optionId": 1,
+            "name": "dfg",
+            "optionName": "gfd",
+            "id": 2,
+            "createdAt": "2023-03-15T01:05:33",
+            "updatedAt": "2023-03-16T01:05:33",
+            "enabled": true
+        }
+    ]
+}
