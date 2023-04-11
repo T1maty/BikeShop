f0729c4133d71dff8b454722e1fe398d447b0e43
Crash2022 <crash555@mail.ru>
2023-04-11 13:17:32 +0500

    add: EndWorkDayModal

---
+++ BikeShop.Frontend/webclient/src/features/EndWorkDayModal/EndWorkDayModal.tsx
@@ -0,0 +1,78 @@
+import React, {useEffect} from 'react'
+import s from './EndWorkDayModal.module.scss'
+import {Button, CustomModal, LoaderScreen} from '../../shared/ui'
+import {useSnackbar} from 'notistack'
+import useEndWorkDayModal from "./EndWorkDayModalStore"
+
+export const EndWorkDayModal = () => {
+
+    const {enqueueSnackbar} = useSnackbar()
+
+    const open = useEndWorkDayModal(s => s.openEndWorkDayModal)
+    const setOpen = useEndWorkDayModal(s => s.setOpenEndWorkDayModal)
+    const isLoading = useEndWorkDayModal(s => s.isLoading)
+    const errorStatus = useEndWorkDayModal(s => s.errorStatus)
+    const getArchive = useEndWorkDayModal(s => s.getArchive)
+
+    useEffect(() => {
+        if (errorStatus === 'error') {
+            enqueueSnackbar('Ошибка сервера', {variant: 'error', autoHideDuration: 3000})
+        }
+    }, [errorStatus])
+
+    useEffect(() => {
+        // getArchive()
+    }, [])
+
+    if (isLoading) {
+        return <LoaderScreen variant={'ellipsis'}/>
+    } else {
+
+        return (
+            <CustomModal
+                open={open}
+                onClose={() => {setOpen(false)}}
+            >
+                <div className={s.endWorkDayModal_mainBlock}>
+                    <div className={s.endWorkDayModal_title}>Закрытие смены</div>
+                    <div className={s.endWorkDayModal_content}>
+                        <div className={s.content_report}>
+                            <div className={s.report}>
+                                Отчёт
+                            </div>
+                            <Button buttonDivWrapper={s.makeVideoBtn}
+                                    // disabled={currentEmployee === null}
+                                    // onClick={() => {setCurrentEmployee(null)}}
+                            >
+                                Снять видео
+                            </Button>
+                        </div>
+                        <div className={s.content_info}>
+                            <div className={s.infoBlock}>
+                                <div>Отработано: 10ч. из 11ч.</div>
+                                <div>Принято ремонтов: 10</div>
+                                <div>Выдано ремонтов: 10</div>
+                                <div>Продаж: 15</div>
+                                <div>Средний чек: 999</div>
+                            </div>
+                            <div className={s.buttonsBlock}>
+                                <Button buttonDivWrapper={s.makeVideoBtn}
+                                    // disabled={currentEmployee === null}
+                                    // onClick={() => {setCurrentEmployee(null)}}
+                                >
+                                    Отмена
+                                </Button>
+                                <Button buttonDivWrapper={s.makeVideoBtn}
+                                    // disabled={currentEmployee === null}
+                                    // onClick={() => {setCurrentEmployee(null)}}
+                                >
+                                    Закрыть смену
+                                </Button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </CustomModal>
+        )
+    }
+}
---
+++ BikeShop.Frontend/webclient/src/features/EndWorkDayModal/EndWorkDayModalStore.ts
@@ -0,0 +1,44 @@
+import {create} from "zustand"
+import {devtools} from "zustand/middleware"
+import {immer} from "zustand/middleware/immer"
+import {ErrorStatusTypes} from "../../entities/enumerables/ErrorStatusTypes"
+
+interface EndWorkDayModalStore {
+    openEndWorkDayModal: boolean
+    setOpenEndWorkDayModal: (value: boolean) => void
+    isLoading: boolean
+    errorStatus: ErrorStatusTypes
+
+    archive: any[]
+    getArchive: () => void
+}
+
+const useEndWorkDayModal = create<EndWorkDayModalStore>()(/*persist(*/devtools(immer((set, get) => ({
+    openEndWorkDayModal: false,
+    setOpenEndWorkDayModal: (value: boolean) => set({
+        openEndWorkDayModal: value
+    }),
+    isLoading: false,
+    errorStatus: 'default',
+
+    archive: [],
+    getArchive: () => {
+        // set({isLoading: true})
+        // ArchiveAPI.getArchive().then((res: any) => {
+        //     set(state => {
+        //         state.archive = res.data
+        //     })
+        //     set({isLoading: false})
+        // }).catch((error: any) => {
+        //     set({errorStatus: 'error'})
+        // }).finally(() => {
+        //     set({errorStatus: 'default'})
+        //     set({isLoading: false})
+        // })
+    },
+})))/*, {
+    name: endWorkDayModalStore",
+    version: 1
+})*/);
+
+export default useEndWorkDayModal;
