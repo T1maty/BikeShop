a4ecfef770e180199aa0cb17107448c412441360
Crash2022 <crash555@mail.ru>
2023-04-12 16:30:47 +0500

    refactor: ShopMain catalog photos

--- BikeShop.Frontend/webclient/src/pages/public/Shop/ShopMain/ShopMain.tsx
+++ BikeShop.Frontend/webclient/src/pages/public/Shop/ShopMain/ShopMain.tsx
@@ -1,22 +1,28 @@
 ﻿import React, {useState} from 'react'
 import s from './ShopMain.module.scss'
 
-import headerPhoto from '../../../../shared/assets/shop/images/header_photo.png'
-import socialLogo from '../../../../shared/assets/shop/icons/logo_instagram.png'
+import HeaderPhoto from '../../../../shared/assets/shop/images/header_photo.png'
+import HeaderPhoto2 from '../../../../shared/assets/shop/images/shop-MainPagePhoto.jpg'
+import SocialLogo from '../../../../shared/assets/shop/icons/logo_instagram.png'
+import Map from '../../../../shared/assets/shop/images/map-01.png'
+import BurgerMenu from '../../../../shared/assets/shop/icons/menu.png'
+import Position from '../../../../shared/assets/shop/icons/position.png'
+import ShopLogo from '../../../../shared/assets/shop/icons/ShopLogo.svg'
+import SearchIcon from '../../../../shared/assets/shop/icons/search.png'
+import Cart from '../../../../shared/assets/shop/icons/cart.png'
+import Language from '../../../../shared/assets/shop/icons/lang.png'
+import Profile from '../../../../shared/assets/shop/icons/profile.png'
+
 import catalogBikes from '../../../../shared/assets/shop/images/catalog-bikes.png'
 import catalogProtection from '../../../../shared/assets/shop/images/catalog-protection.png'
 import catalogSpares from '../../../../shared/assets/shop/images/catalog-spares.png'
+import testImage1 from '../../../../shared/assets/shop/images/testImage-square.png'
+import testImage2 from '../../../../shared/assets/shop/images/testImage-squareBig.png'
+import testImage3 from '../../../../shared/assets/shop/images/testImage-rectangle.png'
 import catalogClothes from '../../../../shared/assets/shop/images/catalog-clothes.png'
 import catalogChemistry from '../../../../shared/assets/shop/images/catalog-chemistry.png'
 import catalogAccessories from '../../../../shared/assets/shop/images/catalog-accessories.png'
 import catalogFood from '../../../../shared/assets/shop/images/catalog-food.png'
-import map from '../../../../shared/assets/shop/images/map-01.png'
-import burgerMenu from '../../../../shared/assets/shop/icons/menu.png'
-import position from '../../../../shared/assets/shop/icons/position.png'
-import searchIcon from '../../../../shared/assets/shop/icons/search.png'
-import cart from '../../../../shared/assets/shop/icons/cart.png'
-import language from '../../../../shared/assets/shop/icons/lang.png'
-import profile from '../../../../shared/assets/shop/icons/profile.png'
 
 import {useNavigate} from 'react-router-dom'
 import {BikeShopPaths} from "../../../../app/routes/paths"
@@ -35,11 +41,11 @@ export const ShopMain = () => {
 
     const [sliderImages, setSliderImages] = useState([
         {
-            original: headerPhoto,
+            original: HeaderPhoto,
             thumbnail: '',
         },
         {
-            original: 'https://s3.amazonaws.com/images.gearjunkie.com/uploads/2018/10/red-bull-rampage-rugged-terrain.jpg',
+            original: HeaderPhoto2,
             thumbnail: '',
         },
     ])
@@ -51,28 +57,26 @@ export const ShopMain = () => {
 
     return (
         <div className={s.shop_wrapper}>
-            {/*<div className={s.shop_mainBox}>*/}
             <div className={s.header}>
-                {/*<div className={s.header_slider}>*/}
-                {/*    <img src={headerPhoto} alt="header-photo"/>*/}
-                {/*</div>*/}
                 <div className={s.header_slider}>
-                    <ImageGallery items={sliderImages}
-                                  showPlayButton={false}
-                                  showFullscreenButton={false}
-                                  showThumbnails={false}
-                                  autoPlay={true}
-                                  slideDuration={3000}
-                    />
+                    <img src={HeaderPhoto} alt="header-photo"/>
                 </div>
+                {/*<div className={s.header_slider}>*/}
+                {/*    <ImageGallery items={sliderImages}*/}
+                {/*                  showPlayButton={false}*/}
+                {/*                  showFullscreenButton={false}*/}
+                {/*                  showThumbnails={false}*/}
+                {/*                  autoPlay={true}*/}
+                {/*                  slideDuration={3000}*/}
+                {/*    />*/}
+                {/*</div>*/}
                 <div className={s.header_icons}>
-                    <div className={s.header_icons_left}>
-                        <div><img src={burgerMenu} alt="burger-menu"/></div>
-                        <div onClick={() => {
-                            navigate(BikeShopPaths.WORKSPACE.MAIN_PAGE)
-                        }}>
-                            <img src={position} alt="position-logo"/>
-                        </div>
+                    <div className={s.header_icons_left}
+                         onClick={() => {
+                             navigate(BikeShopPaths.WORKSPACE.MAIN_PAGE)
+                         }}
+                    >
+                        <img src={ShopLogo} alt="shop-logo"/>
                     </div>
                     <div className={s.header_icons_right}>
                         {
@@ -85,14 +89,16 @@ export const ShopMain = () => {
                         <div onClick={() => {
                             setSearch(!search)
                         }}>
-                            <img src={searchIcon} alt="search-logo"/>
+                            <img src={SearchIcon} alt="search-logo"/>
                         </div>
-
-                        <div><img src={language} alt="language-logo"/></div>
-                        {/*<div><img src={cart} alt="cart-logo"/></div>*/}
+                        <div>
+                            <img src={Language} alt="language-logo"/>
+                        </div>
                         <ShoppingCart/>
-
-                        <LoginBlock isAuth={isAuth} userLastName={userLastName} userFirstName={userFirstName}/>
+                        <LoginBlock isAuth={isAuth}
+                                    userLastName={userLastName}
+                                    userFirstName={userFirstName}
+                        />
                     </div>
                 </div>
             </div>
@@ -101,7 +107,9 @@ export const ShopMain = () => {
                 <div className={s.menu}>
                     <div className={s.container}>
                         <div className={s.menu_items}>
-                            <div onClick={() => {navigate(BikeShopPaths.SHOP.CATALOG)}}>
+                            <div onClick={() => {
+                                navigate(BikeShopPaths.SHOP.CATALOG)
+                            }}>
                                 Каталог
                             </div>
                             <div>Мастерская</div>
@@ -112,42 +120,52 @@ export const ShopMain = () => {
                 </div>
 
                 <div className={s.catalog}>
-                    <div className={s.catalog_group1}>
-                        <div className={s.catalog_item1}>
-                            <img src={catalogBikes} alt="catalog-bikes"/>
-                        </div>
-                        <div className={s.catalog_item2}>
-                            <img src={catalogProtection} alt="catalog-protection"/>
-                        </div>
-                        <div className={s.catalog_item3}>
-                            <img src={catalogSpares} alt="catalog-spares"/>
-                        </div>
-                        <div className={s.catalog_item4}>
-                            <img src={catalogClothes} alt="catalog-clothes"/>
-                        </div>
-                        <div className={s.catalog_item5}>
-                            <img src={catalogChemistry} alt="catalog-chemistry"/>
-                        </div>
-                    </div>
-                    <div className={s.catalog_group2}>
-                        <div className={s.catalog_item}>
-                            <img src={catalogAccessories} alt="catalog-accessories"/>
-                        </div>
-                        <div className={s.catalog_item}>
-                            <img src={catalogFood} alt="catalog-food"/>
-                        </div>
-                    </div>
+                    <div className={s.container}>
 
+                        <div className={s.catalog_items}>
+                            {/*<div className={s.catalog_group1}>*/}
+                                <div className={s.catalog_item}>
+                                    <img src={testImage1} alt="catalog-spares"/>
+                                </div>
+                                <div className={s.catalog_item}>
+                                    <img src={testImage1} alt="catalog-spares"/>
+                                </div>
+                                <div className={s.catalog_item}>
+                                    <img src={testImage1} alt="catalog-spares"/>
+                                </div>
+                                <div className={s.catalog_item}>
+                                    <img src={testImage1} alt="catalog-spares"/>
+                                </div>
+                                <div className={s.catalog_item}>
+
+                                </div>
+                            {/*</div>*/}
+                            {/*<div className={s.catalog_group2}>*/}
+                                <div className={s.catalog_item_big}>
+                                    <img src={testImage1} alt="catalog-accessories"/>
+                                </div>
+                            {/*</div>*/}
+                            {/*<div className={s.catalog_group3}>*/}
+                                <div className={s.catalog_item}>
+                                    <img src={testImage3} alt="catalog-accessories"/>
+                                </div>
+                            {/*</div>*/}
+                            {/*<div className={s.catalog_group4}>*/}
+                                <div className={s.catalog_item}>
+                                    <img src={testImage3} alt="catalog-food"/>
+                                </div>
+                            {/*</div>*/}
+                        </div>
+                    </div>
                 </div>
             </div>
 
             <div className={s.map}>
-                <img src={map} alt="map"/>
+                <img src={Map} alt="map"/>
                 <RegistrationForm/>
             </div>
 
             <ShopFooter/>
-            {/*</div>*/}
         </div>
     )
 }
