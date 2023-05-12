49d5edcfb4e94728ef0e8cf4d8157367ac8afbc5
Crash2022 <crash555@mail.ru>
2023-05-12 13:27:48 +0500

    add: UserRoleModal empty v2

--- BikeShop.Frontend/webclient/src/features/UserRoleModal/UserRoleModal.tsx
+++ BikeShop.Frontend/webclient/src/features/UserRoleModal/UserRoleModal.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import s from './UserRoleModal.module.scss'
-import {Button, CustomModal} from '../../shared/ui'
+import {AsyncSelectSearchProduct, Button, CustomModal} from '../../shared/ui'
 import useUserRoleModal from "./UserRoleModalStore"
+import useCashboxStore from "../../pages/workspace/Cashbox/CashboxStore"
 
 export const UserRoleModal = () => {
 
     const open = useUserRoleModal(s => s.openUserRoleModal)
     const setOpen = useUserRoleModal(s => s.setOpenUserRoleModal)
+
+    const addProduct = useCashboxStore(s => s.addProduct) // temp
 
     return (
         <CustomModal
@@ -17,18 +20,54 @@ export const UserRoleModal = () => {
         >
             <div className={s.userRoleModal_mainBox}>
                 <div className={s.userRoleModal_header}>
-                    <div className={s.header_groups}>1</div>
-                    <div className={s.header_roles}>2</div>
+                    <div className={s.header_groups}>
+                        <div className={s.groups_groups}>
+                            <div>Админ</div>
+                            <div>Менеджер</div>
+                            <div>Разнорабочий</div>
+                        </div>
+                        <Button buttonDivWrapper={s.createGroup}
+                                onClick={() => {}}
+                        >
+                            Создать группу
+                        </Button>
+                    </div>
+                    <div className={s.header_roles}>
+                        <div className={s.roles_description}>1</div>
+                        <div className={s.roles_tabs}>
+                            <div className={s.tabs_toggle}>1</div>
+                            <div className={s.tabs_arrows}>2</div>
+                        </div>
+                        <div className={s.roles_roles}>3</div>
+                    </div>
                 </div>
+
+
                 <div className={s.userRoleModal_bottom}>
-                    <div className={s.bottom_deals}>3</div>
-                    <div className={s.bottom_buttons}>4</div>
+                    <div className={s.bottom_deals}>
+                        <div className={s.deals_select}>
+                            <AsyncSelectSearchProduct onSelect={addProduct}/>
+                        </div>
+                        <div className={s.deals_table}>table</div>
+                    </div>
+                    <div className={s.bottom_buttons}>
+                        <Button buttonDivWrapper={s.applyOne}
+                                onClick={() => {}}
+                        >
+                            Дать пользователю выбранное право
+                        </Button>
+                        <Button buttonDivWrapper={s.applyTwo}
+                                onClick={() => {}}
+                        >
+                            Дать пользователю права выбранной группы
+                        </Button>
+                        <Button buttonDivWrapper={s.close}
+                                onClick={() => {setOpen(false)}}
+                        >
+                            Закрыть
+                        </Button>
+                    </div>
                 </div>
-                {/*<Button onClick={() => {*/}
-                {/*    setOpen(false)*/}
-                {/*}}>*/}
-                {/*    Отмена*/}
-                {/*</Button>*/}
             </div>
         </CustomModal>
     )
